c25bcfef4a100ff428ad59773d7c235b84622360
Javier <elchinatalpf@github.com>
2023-08-29 17:15:42 -0500

    Further work in routes and write file db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,7 @@ var uniqid = require('uniqid');
 // https://www.npmjs.com/package/uniqid
 
   router.get('/api/notes', (req, res) => {
-    fs.readFile(db, (err, data) => {
+    fs.readFile(__dirname + "/../db/db.json", (err, data) => {
       if (err) throw err;
       console.log(JSON.parse(data));
 
@@ -14,21 +14,22 @@ var uniqid = require('uniqid');
     });
   });
 
+  
   router.post('/api/notes', (req, res) => {
+    let userNotes = [];
     let newNote = {
       title: req.body.title,
       text: req.body.text,
       id: uniqid()
     }
-    fs.readFile('./db/db.json', (err, data) => {
+    fs.readFile(__dirname, "../db/db.json", (err, data) => {
       if (err) throw err;
-      let parseData = JSON.parse(data);
-      parseData.push(newNote);
-      console.log(parseData);
+      let userNotes = JSON.parse(data);
+      userNotes.push(newNote);
       
-      fs.writeFile('./db/db.json', JSON.stringify(parseData), (err) => {
+      fs.writeFile(__dirname, "../db/db.json", JSON.stringify(userNotes), "utf-8", (err) => {
         if (err) throw err;
-        res.send('Your NOTE was pushed!');
+        res.send('Your NOTE was saved!');
       });
     });
   });
@@ -36,10 +37,20 @@ var uniqid = require('uniqid');
   router.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
   });
-
+  
   router.post('/api/notes', (req, res) => {
     db.readFileSync('../db/db.json');
-
+    
   });
+  
+  module.exports = router;
 
-module.exports = router;
+
+
+  // router.post("/", (req, res) => {
+  //   try {
+  //     const data = await JSON.parse(fs.write)
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // })
