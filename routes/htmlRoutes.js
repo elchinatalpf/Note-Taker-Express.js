c8043879651f77b75a9d42f86f6b0994cfb55a47
Javier <elchinatalpf@github.com>
2023-08-30 17:32:01 -0500

    Added index.js for routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,13 @@
 const path = require('path');
-const router = require('express').Router();
+const htmlRoutes = require('express').Router();
 
-router.get('/notes', (req, res) => {
+htmlRoutes.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
 
-  router.get('*', (req, res) => {
+  htmlRoutes.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
   });
 
 
-module.exports = router;
+module.exports = htmlRoutes;
