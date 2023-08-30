8b59180065591f10e06da313e2fb14c4278b6028
Javier <elchinatalpf@github.com>
2023-08-29 23:28:36 -0500

    Debbuged and refactored api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,56 +1,44 @@
-const db = require('../db/db.json');
 const fs = require('fs');
 const router = require('express').Router();
 var uniqid = require('uniqid');
 const path = require('path');
 
 
-// https://www.npmjs.com/package/uniqid
-
-  router.get('/api/notes', (req, res) => {
-    fs.readFile (path.join(__dirname, "../db/db.json"), (err, data) => {
-      console.log(JSON.parse(data));
-      
-      res.send(data);
-    });
+  router.get("/api/notes", (req, res) => {
+    
+    res.sendFile(path.join(__dirname, "db/db.json"))
+    
   });
-  
-  
-  router.post('/api/notes', (req, res) => {
+
+  router.post("/api/notes", (req, res) => {
     let newNote = {
       title: req.body.title,
       text: req.body.text,
-      id: uniqid()
+      id: uniqid(),
     };
-    fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+    fs.readFile("db/db.json", (err, data) => {
+
       let userNotes = [];
       if (err) {
-        console.log(err)
+        console.log(err);
       }
       const notesData = JSON.parse(data);
+      if (!data) {
+        data = [];
+      }
       notesData.push(newNote);
       userNotes = JSON.stringify(notesData, null, 2);
-      
-      fs.writeFile(path.join(__dirname, "../db/db.json", userNotes ), (err) => {
+      fs.writeFile("db/db.json", userNotes, (err) => {
         if (err) {
           console.log(err);
         }
-        res.sendFile(path.join(__dirname, './db/db.json'));
       });
+
+      res.sendFile(path.join(__dirname, "db/db.json"));
     });
+
+    console.log(err);
+    res.status(500).send("An error ocurred when reading file.");
   });
-  
 
-
-  
   module.exports = router;
-
-
-
-  // router.post("/", (req, res) => {
-  //   try {
-  //     const data = await JSON.parse(fs.write)
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // })
